652e581f4d09cb8e027b6701caecd7222f912dea
jtouze <jtouze@93126f83-570b-0410-9dce-f3837ab37e36>
2006-11-17 13:00:05 +0000

    qlistederoul.tbl.php, ressource_vote.php  * Remplacement de htmlentities() par emb_htmlentities() pour une meilleure gestion de l'UTF-8.
    git-svn-id: https://svn.cru.fr/esprit/branches/formulaires@224 93126f83-570b-0410-9dce-f3837ab37e36

--- src/database/qlistederoul.tbl.php
+++ src/database/qlistederoul.tbl.php
@@ -244,7 +244,7 @@ class CQListeDeroul
 			}
 			
 			$sCodeHtml.= "<div> Proposition ".$iOrdreTemp.": ";
-			$sCodeHtml.= "\n <input type=\"text\" size=\"60\" maxlength=\"255\" name=\"rep[$IdReponseTemp]\" value=\"".htmlentities($TexteTemp,ENT_COMPAT,"UTF-8")."\" />\n";
+			$sCodeHtml.= "\n <input type=\"text\" size=\"60\" maxlength=\"255\" name=\"rep[$IdReponseTemp]\" value=\"".emb_htmlentities($TexteTemp)."\" />\n";
 			$sCodeHtml.= "<select name=\"selOrdreProposition[$IdReponseTemp]\">".$sCodeOptionsOrdre."</select>";
 			if($v_bAutoCorrection)
 			{
