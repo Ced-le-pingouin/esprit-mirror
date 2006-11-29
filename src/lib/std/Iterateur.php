bc5d9d6b3493a8dfff9f1f8365dcb70044e8be50
cedric_floquet <cedric_floquet@93126f83-570b-0410-9dce-f3837ab37e36>
2006-11-29 21:53:17 +0000

    * il y avait eu un problème dans mon commit précédent: le fichier Iterateur.php   n'était pas dans sa bonne (nouvelle) version. C'est ok maintenant
    git-svn-id: https://svn.cru.fr/esprit/trunk@254 93126f83-570b-0410-9dce-f3837ab37e36

--- src/lib/std/Iterateur.php
+++ src/lib/std/Iterateur.php
@@ -21,26 +21,25 @@
 
 /**
  * @file	Iterateur.php
- *
- * Contient une classe abstraite pour l'implémentation d'itérateurs en PHP 4 et +
  */
 
 require_once(dirname(__FILE__).'/OO.php');
 
 /**
- * Classe abstraite qui présente les méthodes requise pour implémenter un itérateur.
+ * Interface qui définit les méthodes requises par un itérateur "unidirectionnel" simple.
  * C'est également ici que se trouve la documentation sur le comportement attendu des implémentations.
  */
 class Iterateur
 {
 	/**
-	 * Constructeur. Celui de cette classe n'est pas opérationnel et provoque une erreur car il s'agit d'un semblant de
-	 * classe abstraite/interface en PHP 4, qui ne devrait pas être utilisée ou instanciée telle quelle.
+	 * Constructeur
 	 *
-	 * Par contre, les sous-classes devraient, dans leur constructeur, initialiser l'objet à itérer, car l'itérateur
-	 * devrait être positionné au début dès sa création
+	 * @param	v_oObjetAIterer	l'objet sur lequel aura lieu l'itération. Son type dépendra bien entendu de
+	 * 			l'implémentation
+	 *
+	 * @note	Selon l'implémentation, des paramètres supplémentaires pourront être acceptés ou requis
 	 */
-	function Iterateur()
+	function Iterateur($v_oObjetAIterer)
 	{
 		OO::abstraite();
 	}
@@ -54,7 +53,7 @@ class Iterateur
     }
 
     /**
-     * Déplace l'itérateur vers l'avant
+     * Déplace l'itérateur d'une position vers l'avant
      */
     function suiv()
     {
@@ -95,47 +94,20 @@ class Iterateur
 
     /**
      * Place l'itérateur sur le dernier élément
-     *
-     * @warning	Même remarque sur la performance que pour #taille()
      */
     function fin()
     {
-    	$this->debut();
-
-    	$iPosDernier = $this->taille() - 1;
-    	for ($i = 0; $i < $iPosDernier; $i++)
-			$this->suiv();
+		OO::abstraite();
     }
-
-    /**
-     * Déplace l'itérateur vers l'arrière
-     */
-    function prec()
-    {
-    	OO::abstraite();
-    }
-
 
     /**
      * Retourne le nombre d'éléments présents dans l'itérateur
      *
      * @return	le nombre d'éléments de l'itérateur
-     *
-     * @warning	l'implémentation par défaut de cette méthode, présente dans la classe abstraite Iterateur, parcourt
-     * 			l'itérateur avec une boucle PHP pour accomplir sa tâche, ce qui peut être très très lent sur de grandes
-     * 			"collections"; donc quand il est possible d'utiliser des fonctions natives directes ou plus rapides pour
-     * 			implémenter cette méthode dans une sous-classe d'Iterateur, il est vivement conseillé de la redéfinir
      */
     function taille()
     {
-		$CleSauvee = $this->cle();
-
-		for ($this->debut(), $iTaille = 0; $this->estValide(); $this->suiv())
-			++$iTaille;
-
-		$this->rechercher($CleSauvee);
-
-		return $iTaille;
+		OO::abstraite();
     }
 
     /**
@@ -146,80 +118,33 @@ class Iterateur
      *
      * @return	\c true si la clé recherchée existe bien dans la "collection" sur laquelle l'itérateur fonctionne,
      * 			\c false sinon
-     *
-     * @warning	Même remarque sur la performance que pour #taille()
      */
     function rechercher($v_Cle)
     {
-    	$CleSauvee = $this->cle();
-
-    	for ($this->debut(); $this->estValide(); $this->suiv())
-    		if ($this->cle() === $v_Cle)
-    			return TRUE;
-
-		// si pas trouvé, on replace l'itérateur sur la position qu'il avait avant la recherche, SAUF si cette position
-		// était identique en début de recherche à celle en fin => = position "invalide" car après le dernier élément
-		// => si on demande à chercher cette position invalide, elle ne sera jamais trouvée, et provoquera un nouvel
-		// appel à rechercher() à la fin de chaque appel rechercher() etc. => boucle infinie
-		if ($this->cle() !== $CleSauvee)
-			$this->rechercher($CleSauvee);
-		return FALSE;
+		OO::abstraite();
     }
 
     /**
      * Indique si l'élément courant de l'itérateur est le premier
      *
      * @return	\c true si l'itérateur est positionné sur le premier élément. Sinon \c false
-     *
-     * @warning	Même remarque sur la performance que pour #taille()
      */
     function estPremier()
     {
-    	// on sauvegarde la clé actuelle de l'itérateur et on le place au début...
-    	$CleSauvee = $this->cle();
-    	$this->debut();
-
-    	// ...pour vérifier si la clé actuelle était bien celle du premier élément
-    	if ($this->cle() === $CleSauvee)
-    	{
-    		return TRUE;
-    	}
-    	// sinon, ça n'était pas le 1er élément, alors on replace l'itérateur dans sa position d'avant l'appel
-    	else
-    	{
-    		$this->rechercher($CleSauvee);
-    		return FALSE;
-    	}
+		OO::abstraite();
     }
 
     /**
      * Indique si l'élément courant de l'itérateur est le dernier
      *
      * @return	\c true si l'itérateur est positionné sur le dernier élément. Sinon \c false
-     *
-     * @warning	Même remarque sur la performance que pour #taille()
      */
     function estDernier()
     {
-    	// on sauvegarde la clé actuelle de l'itérateur et on le place à la fin...
-    	$CleSauvee = $this->cle();
-    	$this->fin();
-
-    	// ...pour vérifier si la clé actuelle était bien celle du dernier élément
-    	if ($this->cle() === $CleSauvee)
-    	{
-    		return TRUE;
-    	}
-    	// sinon, ça n'était pas le dernier élément, alors on replace l'itérateur dans sa position d'avant l'appel
-    	else
-    	{
-    		$this->rechercher($CleSauvee);
-    		return FALSE;
-    	}
+		OO::abstraite();
     }
 }
 
-// n'est pas une interface, car certaines méthodes sont prédéfinies
-OO::defClasseAbstraite();
+OO::defInterface();
 
 ?>
