7016e1211e57162684512644e5a0b073037e767c
fporco <fporco@93126f83-570b-0410-9dce-f3837ab37e36>
2007-03-23 08:43:22 +0000

    * Corrigé un problème javascript dans le pliage/dépliage de la liste des inscrits (zone de droite) ; + inscription-inscrits.htm a maintenant son propre fichier javascript.
    git-svn-id: https://svn.cru.fr/esprit/trunk@367 93126f83-570b-0410-9dce-f3837ab37e36

--- src/admin/associer/inscription.js
+++ src/admin/associer/inscription.js
@@ -1,19 +1,3 @@
-function visible(v_elem) {
-	for (var i=0; i<v_elem.childNodes.length; i++)
-	{
-		if ( v_elem.childNodes.item(i).nodeName == "DT"
-			|| v_elem.childNodes.item(i).nodeName == "DD") {
-			if (v_elem.childNodes.item(i).className == "replier")
-				v_elem.childNodes.item(i).className = "deplier";
-			else
-				v_elem.childNodes.item(i).className = "replier";
-		}
-		
-		if (v_elem.childNodes.item(i).nodeName == "DD")
-			break;
-	}
-}
-
 function rechargerListeInscrits(v_iNouveauStatut) {
 	with (document.getElementById("recharger_liste_inscrits")) {
 		elements["idstatut"].value = v_iNouveauStatut;
