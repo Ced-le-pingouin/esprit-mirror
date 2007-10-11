3e4ee0b2bb7c9416db26e6cb4ddefe2988d9f692
gannaz <gannaz@93126f83-570b-0410-9dce-f3837ab37e36>
2007-10-11 13:06:12 +0000

    import : désactive .ods, buggé
    git-svn-id: https://svn.cru.fr/esprit/branches/2.3-nav@484 93126f83-570b-0410-9dce-f3837ab37e36

--- src/admin/personne/import.php
+++ src/admin/personne/import.php
@@ -177,7 +177,7 @@ Télécharger le modèle de feuille de tableur
 <ul>
   <li>au format <a href="esprit_inscriptions.xls">Excel</a></li>
   <li>au format <a href="esprit_inscriptions.csv">CSV</a></li>
-  <li>au format <a href="esprit_inscriptions.ods">ODS</a></li>
+<?php /* BUGS IN PARSER <li>au format <a href="esprit_inscriptions.ods">ODS</a></li> */ ?>
 </ul>
 <p>Attention à ne pas modifier les <strong>5 premières lignes</strong> de ces modèles.</p>
 <p>Le fichier CSV doit utiliser le jeu de caractères <em>UTF-8</em>. Si les accents des permières lignes s'affichent mal, ce n'est pas le cas.</p>
