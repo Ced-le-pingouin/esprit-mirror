559d98677733f64d862d222908878a83c299a54a
gannaz <gannaz@93126f83-570b-0410-9dce-f3837ab37e36>
2007-12-12 17:51:11 +0000

    BRANCHE hotpotatoes : pb de charset (typo)
    git-svn-id: https://svn.cru.fr/esprit/branches/hotpotatoes@518 93126f83-570b-0410-9dce-f3837ab37e36

--- src/sousactiv/hotpotatoes/scores.tpl.php
+++ src/sousactiv/hotpotatoes/scores.tpl.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset="utf-8">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <title>Scores</title>
 <style type="text/css">
 td { padding: 3px 1ex; }
@@ -13,7 +13,7 @@ td { padding: 3px 1ex; }
 <h3>Etudiant : <em><?php echo $oEtudiant->retNomComplet(); ?></em></h3>
 <table border="1">
 	<thead>
-		<tr><td>Date de début de l'exercice</td><td>Date de soumission</td><td>Exercice fini</td><td>Score</td></tr>
+		<tr><td>Date de dÃ©but de l'exercice</td><td>Date de soumission</td><td>Exercice fini</td><td>Score</td></tr>
 	</thead>
 	<tbody>
 <?php
