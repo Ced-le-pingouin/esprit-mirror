87b4f3b0674c73947b2f19c23fdfa0b9cf58f93f
gannaz <gannaz@93126f83-570b-0410-9dce-f3837ab37e36>
2008-01-09 15:35:52 +0000

    BRANCHE hotpotatoes : chgt graphique ds le tableau détaillé des scores
    git-svn-id: https://svn.cru.fr/esprit/branches/hotpotatoes@525 93126f83-570b-0410-9dce-f3837ab37e36

--- src/sousactiv/hotpotatoes/scores.tpl.php
+++ src/sousactiv/hotpotatoes/scores.tpl.php
@@ -4,13 +4,16 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <title>Scores</title>
 <style type="text/css">
+html { font-family: Arial; }
 table { empty-cells: hide; border-collapse: collapse; background-color: white; border: 2px solid black; }
 th, td { padding: 3px 1.5ex; border: 1px solid black; border-top: 1px dotted black; border-bottom: 1px dotted black; }
 td.score { text-align: right; }
-th { background-color: #E0E0E0; border-bottom: 2px solid black; }
-tr.fini td { background-color: #A0F0F0; border-bottom: 2px solid black; }
+th { background-color: rgb(208,208,191); border-bottom: 2px solid black; }
+tr.fini td { background-color: rgb(251,249,238); border-bottom: 2px solid black; }
 tr.fini td.date { background-color: white; }
-tr.inter td.date { border-bottom: hidden; }
+tr.suite td.date { border-top: hidden; }
+tr:hover,
+tr.fini:hover td { background-color: rgb(238,234,221); }
 </style>
 </head>
 <body>
@@ -30,9 +33,9 @@ if (empty($oHotpotScores)) {
 <?php
 $derDate = 0;
 foreach ($oHotpotScores as $oScore) {
-	echo '<tr class="' .($oScore->retFini() ? 'fini' : 'inter') .'">'
-		.'<td class="date">'.( $oScore->retDateDebut()===$derDate ?
-			'' : date('d/m/Y H:i',$oScore->retDateInitiale()) ).'</td>'
+	$suite = ($oScore->retDateDebut()===$derDate ? true : false);
+	echo '<tr class="' .($oScore->retFini() ? 'fini' : 'inter') . ($suite?' suite':'') .'">'
+		.'<td class="date">'.( $suite ? '' : date('d/m/Y H:i',$oScore->retDateInitiale()) ).'</td>'
 		.'<td>'.$oScore->retDuree().'</td>'
 		.'<td>'.($oScore->retFini() ? 'Fini' : "Intermédiaire").'</td>'
 		.'<td class="score">'.$oScore->retScore().' %</td>'
