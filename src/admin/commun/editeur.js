6b399497bb809414dd669bd4a26460d17e7191c9
loic <loic@93126f83-570b-0410-9dce-f3837ab37e36>
2008-05-14 12:45:41 +0000

    Configuration éditeur pour améliorer le copier/coller depuis word : 	+ ajout d'une fonction spécifique qui enlève les balises spécifiques à IE et FF, 		et nettoyage des balises.
    git-svn-id: https://svn.cru.fr/esprit/trunk@568 93126f83-570b-0410-9dce-f3837ab37e36

--- src/admin/commun/editeur.js
+++ src/admin/commun/editeur.js
@@ -12,28 +12,80 @@ function initEditeur( Mode, Elements, tableauDeBord ) {
 	}
 tinyMCE.init({
 	theme : "advanced",
-	plugins : "save,advhr,advlink,emotions,insertdatetime,zoom,searchreplace,contextmenu,directionality"
+	plugins : "save,advhr,advimage,advlink,insertdatetime,zoom,searchreplace,contextmenu,directionality,paste"
 		+(tableauDeBord?",tableaubord":""),
-// theme_advanced_buttons1_add_before : "save,separator",
-theme_advanced_buttons1_add : "fontselect,fontsizeselect,forecolor,backcolor",
-theme_advanced_buttons2_add : "separator,zoom",
-theme_advanced_buttons2_add_before: "cut,copy,paste,separator,search,replace,separator",
-theme_advanced_disable : "image",
-theme_advanced_buttons2_add : "separator,ltr,rtl,separator,"
-	+(tableauDeBord?"tableaubordi,tableauborde,separator,":"")
-	+"iespell,hr,removeformat,sub,sup,charmap,visualaidseparator,print",
-theme_advanced_buttons3: "",
-theme_advanced_toolbar_location : "top",
-theme_advanced_toolbar_align : "left",
-theme_advanced_path_location : "bottom",
-plugin_insertdate_dateFormat : "%Y-%m-%d",
-plugin_insertdate_timeFormat : "%H:%M:%S",
-extended_valid_elements : "a[name|href|target|title|onclick],img[class|src|border=0|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name],hr[class|width|size|noshade],font[face|size|color|style],span[class|align|style]",
+
+	theme_advanced_buttons1_add : "fontselect,fontsizeselect,forecolor,backcolor",
+	theme_advanced_buttons2_add_before: "cut,copy,pasteword,separator,search,replace,separator",
+	theme_advanced_buttons2_add : "separator,ltr,rtl,separator,"
+		+(tableauDeBord?"tableaubordi,tableauborde,separator,":"")
+		+"iespell,hr,removeformat,sub,sup,charmap",
+	theme_advanced_disable : "image",
+	theme_advanced_buttons3: "",
+	theme_advanced_toolbar_location : "top",
+	theme_advanced_toolbar_align : "left",
+	theme_advanced_path_location : "bottom",
+
+	paste_create_paragraphs : true,
+	paste_create_linebreaks : false,
+	paste_remove_styles: false,
+	paste_remove_spans: false,
+	paste_use_dialog : true,
+	paste_auto_cleanup_on_paste : false,
+	paste_convert_middot_lists : true,
+	paste_unindented_list_class : "unindentedList",
+	paste_convert_headers_to_strong : false,
+	paste_strip_class_attributes : 'mso',
+	paste_insert_word_content_callback : "convertWord",
+	
+	force_p_newlines : true,
+	force_br_newlines : false,
+	forced_root_block : 'p',
+	convert_fonts_to_spans : false,
+	fix_content_duplication : true,
+	fix_list_elements : true,
+	fix_table_elements : true,
+	fix_nesting : true,
+	preformatted : false,
+	
+	plugin_insertdate_dateFormat : "%Y-%m-%d",
+	plugin_insertdate_timeFormat : "%H:%M:%S",
+	extended_valid_elements : "a[name|href|target|title|onclick],img[class|src|border=0|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name],hr[class|width|size|noshade],font[face|size|color|style],span[class|align|style]",
+	invalid_elements : "p[style]",
 	mode : Mode,
 	elements : Elements,
 	setupcontent_callback : contentCallback,
 	onchange_callback : onchangeCallback,
 	language : "fr",
-	docs_language : "fr" // doc fr -> vérifiée
+	docs_language : "fr"
 });
+}
+function convertWord(type, content) {
+	content = content.replace(/-moz-use-text-color/gi, ""); // on enlève les balises spéciales créées par Mozilla/FireFox
+	content = content.replace(/( line-height: )[a-z]*(;){1,}/gi, "");
+	content = content.replace(/\s?mso-[^\"]*/gi, ""); // on enlève les balises spéciales créées par IE
+	
+	// enleve les balises à l'intérieur d'un 'paragraphe' vide, sinon bug sur les tableaux
+	var recherche_p = /<p(\s?[^>]*)>((<[^>]*>)*).?nbsp.?((<[^>]*>)*)<\/p>/gi;
+	recherche_p.exec(content);
+	content = content.replace(recherche_p,"<p"+RegExp.$1+">&nbsp;</p>");
+	
+	recherche_td = content.match(/(td).+(border).{0,7}(:)[a-z0-9\s]*(; )/gi); // recherche des <td...></td>
+	if (recherche_td)
+	{
+		for(i=0;i<recherche_td.length;++i)
+		{
+			content = content.replace(/(border-).{1,7}(:)[a-z0-9\s]*(; )/gi, ""); // enleve les style 'border-color'... dans le td
+		}
+	}
+	recherche_tab = content.match(/(table).+(border).{0,7}(:)[a-z0-9\s]*(; )/gi); // recherche des <table...> contenant un style border
+	if (recherche_tab)
+	{
+		for(i=0;i<recherche_tab.length;++i)
+		{
+			content = content.replace(/(border).{0,}(:)[a-z0-9\s]*((;)|(; ))/gi, ""); // enleve les style 'border-color'... dans la table
+		}
+	}
+
+	return content;
 }
