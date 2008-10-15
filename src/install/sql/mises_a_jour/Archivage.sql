509154a2da683b1613dae8317856dfcf0d762f93
loic <loic@93126f83-570b-0410-9dce-f3837ab37e36>
2008-10-15 07:39:41 +0000

    Ajout du fichier 'archivage.sql' dans le répertoire 'install/mises_a_jour/'
    Correction d'une faute de frappe dans le copier/coller de formation.
    
    git-svn-id: https://svn.cru.fr/esprit/trunk@684 93126f83-570b-0410-9dce-f3837ab37e36

---
+++ src/install/sql/mises_a_jour/Archivage.sql
@@ -0,0 +1,12 @@
+/* ajout d'un statut dans les session : 'archivée' */
+UPDATE `Permission` SET `DescrPermis` = 'Peut changer le statut de n''importe quelle session (ouverte,fermée,invisible,archivée)' WHERE `permission`.`IdPermission` =12 LIMIT 1 ;
+UPDATE `Permission` SET `DescrPermis` = 'Peut changer le statut de "sa" session (ouverte,fermée,invisible,archivée)' WHERE `permission`.`IdPermission` =13 LIMIT 1 ;
+UPDATE `Permission` SET `DescrPermis` = 'Modère les forum de la session' WHERE `permission`.`IdPermission` =62 LIMIT 1 ;
+
+/* ajout des permissions pour les archives */
+INSERT INTO `Permission` (`IdPermission`, `NomPermis`, `DescrPermis`) VALUES (NULL, 'PERM_VOIR_SESSION_ARCHIVES', 'Peut voir et accéder aux formations archivées');
+INSERT INTO `Permission` (`IdPermission` ,`NomPermis` ,`DescrPermis`) VALUES (NULL, 'PERM_MOD_ARCHIVES', 'Peut modifier une formation archivée');
+
+/* ajout des permissions pour les différents statuts */
+INSERT INTO `Statut_Permission` (`IdPermission`, `IdStatut`) VALUES ('106', '1'), ('106', '2'), ('106', '3'), ('106', '5'), ('106', '7'); /* voir les sessions archivées */
+
