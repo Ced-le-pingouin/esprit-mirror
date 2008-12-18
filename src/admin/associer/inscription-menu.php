242cf910d1cd970a1e9e1e83d9a96bd68e61231b
loic <loic@93126f83-570b-0410-9dce-f3837ab37e36>
2008-12-18 17:31:29 +0000

    Correction du rafraîchissement des frames 'FRM_PERSONNE' et 'FRM_INSCRIT'. 	Le rechargement se faisait sur la page entière 	->perte des données de l'url.
    git-svn-id: https://svn.cru.fr/esprit/trunk@729 93126f83-570b-0410-9dce-f3837ab37e36

--- src/admin/associer/inscription-menu.php
+++ src/admin/associer/inscription-menu.php
@@ -5,7 +5,7 @@ $url_iIdFormation = (empty($_GET["idForm"]) ? NULL : stripslashes($_GET["idForm"
 $sBlockHead = NULL;
 $aMenus   = array();
 $aMenus[] = array(_("Changer de formation"),"top.choix_formation('{$url_sTitrePrincipal}','{$url_iIdFormation}')",1,"text-align: left;");
-$aMenus[] = array(_("Rafra&icirc;chir"),"top.frames['Principal'].location.reload(true)",3,"text-align: right");
+$aMenus[] = array(_("Rafra&icirc;chir"),"top.frames['Principal'].frames['FRM_PERSONNE'].location.reload(true);top.frames['Principal'].frames['FRM_INSCRIT'].location.reload(true);",3,"text-align: right");
 $aMenus[] = array(_("Fermer"),"top.close()",2);
 include_once(dir_template("dialogue","dialog-menu.tpl.php"));
 ?>
