5928213cc980fe30de35d21dd790dfbec585a40b
cedric_floquet <cedric_floquet@93126f83-570b-0410-9dce-f3837ab37e36>
2010-10-26 09:43:00 +0000

    TemplateEtendu provoque une exception si le fichier template est inaccessible
    Auparavant, la page affichée était complètement vide si le fichier template 
    n'existait pas, et il n'y avait aucun message d'erreur.
    
    git-svn-id: https://svn.cru.fr/esprit/branches/stable-2.3-late-fixes@817 93126f83-570b-0410-9dce-f3837ab37e36

--- src/lib/std/TemplateEtendu.php
+++ src/lib/std/TemplateEtendu.php
@@ -3,6 +3,15 @@ require_once dirname(__FILE__).'/../../include/template.inc.php';
 
 class TemplateEtendu extends Template
 {
+	public function __construct($fichierTemplate)
+	{
+		if (!is_readable($fichierTemplate)) {
+			throw new Exception("Fichier template '{$fichierTemplate}' inaccessible!");
+		}
+		
+		parent::Template($fichierTemplate);
+	}
+	
 	/**
 	 * @param string $motif
 	 * @param callback $callback
