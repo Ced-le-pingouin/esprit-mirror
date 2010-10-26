2fb8801ef40d146fcbb06904a22beb6b7ddad1ea
cedric_floquet <cedric_floquet@93126f83-570b-0410-9dce-f3837ab37e36>
2010-10-26 14:49:15 +0000

    Possibilité d'inclure des "sous-templates" dans un TemplateEtendu
    Il faut pour ça écrire, n'importe où dans le template principal, la commande 
    "[sub:<nom-sous-template>]". L'extension sera automatiquement ".tpl".
    Si un fichier portant ce nom est disponible, il sera automatiquement inclus.
    
    Dans le constructeur, un second paramètre booléen facultatif permet de 
    déterminer si les commandes d'inclusion de sous-templates doivent être 
    automatiquement exécutées à la création d'un template. Ce paramètre est à true
    par défaut.
    
    Si on décide de ne pas exécuter automatiquement les inclusions de 
    sous-templates, on pourra les déclencher ultérieurement en appelant la méthode
    inclureSousTemplates() sur l'objet TemplateEtendu.
    
    Voici un exemple d'inclusion de sous-template dans un template principal:
    
    <html>
    <head>...</head>
    <body>
    
    Contenu principal
    
    [sub:mon_sous_template]
    
    ...
    </body>
    </html>
    
    Dans l'exemple ci-dessus, le contenu du fichier nommé "mon_sous_template.tpl"
    sera automatiquement inclus à l'endroit de la commande "[sub:...]".
    Le sous-template devrait se trouver dans le même dossier que le template 
    principal.
    
    git-svn-id: https://svn.cru.fr/esprit/branches/stable-2.3-late-fixes@820 93126f83-570b-0410-9dce-f3837ab37e36

--- src/lib/std/TemplateEtendu.php
+++ src/lib/std/TemplateEtendu.php
@@ -3,13 +3,48 @@ require_once dirname(__FILE__).'/../../include/template.inc.php';
 
 class TemplateEtendu extends Template
 {
-	public function __construct($fichierTemplate)
+	const EXTENSION_PAR_DEFAUT = 'tpl';
+	
+	/**
+	 * @param string $fichierTemplate
+	 * @param boolean $inclureSousTemplatesAuto
+	 */
+	public function __construct($fichierTemplate, $inclureSousTemplatesAuto = true)
 	{
 		if (!is_readable($fichierTemplate)) {
 			throw new Exception("Fichier template '{$fichierTemplate}' inaccessible!");
 		}
 		
 		parent::Template($fichierTemplate);
+		
+		if ($inclureSousTemplatesAuto) {
+		  $this->inclureSousTemplates();
+		}
+	}
+	
+	public function inclureSousTemplates()
+	{
+		$this->remplacerParMotifEtCallback(
+		    '/\\[sub:([\\w.]+)\\]/',
+		    array($this, 'remplacerCommandeSousTemplateParContenuFichier')
+		);
+	}
+	
+	/**
+	 * @param array[]string $correspondances
+	 * @throws Exception
+	 */
+	protected function remplacerCommandeSousTemplateParContenuFichier($correspondances)
+	{
+		list($tout, $nomSousTemplate) = $correspondances;
+		
+		$fichierSousTemplate = $nomSousTemplate.'.'.self::EXTENSION_PAR_DEFAUT;
+		
+		if (!is_readable($fichierSousTemplate)) {
+			throw new Exception("Le sous-template '{$fichierSousTemplate}' n'a pu être inclus car il est inaccessible");
+		}
+		
+		return file_get_contents($fichierSousTemplate);
 	}
 	
 	/**
