84724ee74aa43d3327c0737cd91ffcbc65826181
cedric_floquet <cedric_floquet@93126f83-570b-0410-9dce-f3837ab37e36>
2010-11-26 10:09:23 +0000

    WIP!!! PAS TERMINÉ!!! : classes pour la validation automatique de collecticiels
    Une classe "validateur" est instanciée en lui passant l'objet CSousActiv sur 
    lequel on souhaite effectuer une validation automatique. D'ailleurs, 
    le constructeur est implémenté de cette façon et est "final" => ne peut être
    modifié.
    
    Il existe une classe abstraite, de base, qui force l'implémentation de 2 
    méthodes de validation: une pour valider tous les documents soumis à partir
    d'un date donnée, et une pour valider les X derniers documents d'une même 
    personne dans la sous-activité.
    
    Seules les sous-activités de type collecticiel et formulaire seront supportées.
    Pour l'instant, la classe pour collecticiels est implémentée.
    
    Dans ce WIP, quelques lignes sont encore commentées (l'update final ne se fait
    pas), et des instructions de test se trouvent à la fin du fichier de la classe.
    
    git-svn-id: https://svn.cru.fr/esprit/branches/stable-2.3-late-fixes@846 93126f83-570b-0410-9dce-f3837ab37e36

---
+++ src/include/ValidateurAutomatiqueCollecticiel.php
@@ -0,0 +1,177 @@
+<?php
+require_once dirname(__FILE__).'/ValidateurAutomatiqueSousActivite.php';
+
+class ValidateurAutomatiqueCollecticiel extends ValidateurAutomatiqueSousActivite
+{
+	/**
+     * @param int $jour
+     * @param int $mois
+     * @param int $annee
+     * 
+     * @return int
+     */
+	public function validerAPartirDeDate($jour, $mois, $annee)
+	{
+		$ids = $this->trouverIdsRessourcesSoumisesAPartirDeDate(
+		    $jour, $mois, $annee
+		);
+		$this->validerRessourcesParIds($ids);
+		
+		return count($ids);
+	}
+	
+	/**
+	 * @param int $combien
+	 * 
+	 * @return int
+	 */
+	public function validerXDerniers($combien = 1)
+	{
+        $ids = $this->trouverIdsXDernieresRessourcesSoumisesParPersonne(
+            $combien
+        );
+        $this->validerRessourcesParIds($ids);
+        
+        return count($ids);
+	}
+	
+    /**
+     * @param int $jour
+     * @param int $mois
+     * @param int $annee
+     * 
+     * @return array[int]int
+     */
+    private function trouverIdsRessourcesSoumisesAPartirDeDate($jour, $mois, $annee)
+    {
+        $sqlIntermediaire = "
+            SELECT
+              Ressource_SousActiv.IdResSousActiv AS id
+            FROM
+              Ressource_SousActiv
+              INNER JOIN Ressource USING (IdRes)
+            WHERE
+              Ressource_SousActiv.IdSousActiv='%d'
+              AND Ressource_SousActiv.StatutResSousActiv='%d'
+              AND Ressource.DateRes >= '%4d-%02d-%02d'
+        ";
+        
+        $variablesSql = array(
+            $this->sousActivite->retId(),
+            STATUT_RES_SOUMISE,
+            (int)$annee, (int)$mois, (int)$jour
+        );
+        
+        $sqlFinal = vsprintf($sqlIntermediaire, $variablesSql);
+        
+        return $this->retTableauIdsAPartirRequete($sqlFinal);
+    }
+	
+	/**
+	 * @param int $combien
+	 * 
+	 * @return array[int]int 
+	 */
+	private function trouverIdsXDernieresRessourcesSoumisesParPersonne($combien)
+	{
+		$sqlIntermediaire = "
+            SELECT
+              rsa.IdResSousActiv AS id
+            FROM
+              Ressource_SousActiv AS rsa
+              INNER JOIN Ressource AS r USING (IdRes)
+            WHERE
+              rsa.IdSousActiv='%1\$d'
+              AND rsa.StatutResSousActiv='%2\$d'
+              AND 
+              (
+                /* 
+                 on compte le nombre de documents déposés par la même personne 
+                 dans cette activité, A PARTIR DE celui traité dans la requête 
+                 externe... 
+                */ 
+                SELECT
+                  COUNT(*)
+                FROM
+                  Ressource_SousActiv AS rsa2
+                  INNER JOIN Ressource AS r2 USING (IdRes)
+                WHERE
+                  rsa2.IdSousActiv='%1\$d'
+                  AND rsa2.StatutResSousActiv='%2\$d'
+                  AND r.IdPers=r2.IdPers
+                  /* NOTE: pour prendre les X _premiers_, remplacer >= par <= */
+                  AND r2.DateRes >= r.DateRes
+              /*
+               ...donc, si le document est bien dans les X derniers, il est 
+               gardé dans les résultats 
+              */
+              ) <= '%3\$d'
+        ";
+        
+        $variablesSql = array(
+            $this->sousActivite->retId(),
+            STATUT_RES_SOUMISE,
+            $combien
+        );
+        
+        $sqlFinal = vsprintf($sqlIntermediaire, $variablesSql);
+        
+        return $this->retTableauIdsAPartirRequete($sqlFinal);
+	}
+	
+	/**
+	 * @param string $requete
+	 * 
+	 * @return array[int]int
+	 */
+	private function retTableauIdsAPartirRequete($requete)
+	{
+		$db = $this->sousActivite->oBdd;
+        $resultat = $db->executerRequete($requete);
+        
+        $ids = array();
+        while (false !== ($ligne = $db->retEnregSuiv($resultat))) {
+            $ids[] = (int)$ligne->id;
+        }
+        $db->libererResult($resultat);
+
+        return $ids;
+	}
+	
+	/**
+	 * @param int|array[]int $ids
+	 */
+	private function validerRessourcesParIds($ids)
+	{
+		$ids = (array)$ids;
+		if (count($ids) <= 0) {
+			return;
+		}
+		
+		$listeIds = implode(',', $ids);
+		
+		$sqlIntermediaire = "
+            UPDATE
+              Ressource_SousActiv
+            SET
+              StatutResSousActiv='%d'
+            WHERE
+              IdResSousActiv IN (%s)
+		";
+		
+		$variablesSql = array(
+		    STATUT_RES_ACCEPTEE,
+		    $listeIds
+		);
+		
+		$sqlFinal = vsprintf($sqlIntermediaire, $variablesSql);
+		
+		//$this->sousActivite->oBdd->executerRequete($sqlFinal);
+	}
+}
+
+$projet = new CProjet();
+$sousActivite = new CSousActiv($projet->oBdd, 13980);
+
+$validateur = ValidateurAutomatiqueSousActivite::creer($sousActivite);
+echo PHP_EOL . $validateur->validerXDerniers(1) . PHP_EOL;
---
+++ src/include/ValidateurAutomatiqueSousActivite.php
@@ -0,0 +1,53 @@
+<?php
+require_once dirname(__FILE__).'/../globals.inc.php';
+
+abstract class ValidateurAutomatiqueSousActivite
+{
+	/** @var CSousActiv */
+	protected $sousActivite;
+	
+	/**
+	 * @param CSousActiv $sousActivite
+	 */
+	final protected function __construct(CSousActiv $sousActivite)
+	{
+		$this->sousActivite = $sousActivite;
+	} 
+	
+	/**
+	 * @param CSousActiv $sousActivite
+	 * @return ValidateurAutomatiqueSousActivite
+	 * @throws Exception
+	 */
+    public static function creer(CSousActiv $sousActivite)
+    {
+    	$typeSousActivite = $sousActivite->retType();
+    	
+    	switch($typeSousActivite) {
+    		case LIEN_COLLECTICIEL:
+    			return new ValidateurAutomatiqueCollecticiel($sousActivite);
+    			break;
+    		case LIEN_FORMULAIRE:
+    			return new ValidateurAutomatiqueFormulaire($sousActivite);
+    			break;
+    		default:
+    			throw new Exception("La sous-activité à valider doit être de type collecticiel ou formulaire");
+    	}
+    }
+    
+    /**
+     * @param int $jour
+     * @param int $mois
+     * @param int $annee
+     * 
+     * @return int
+     */
+    abstract public function validerAPartirDeDate($jour, $mois, $annee);
+    
+    /**
+     * @param int $combien
+     * 
+     * @return int
+     */
+    abstract public function validerXDerniers($combien = 1);
+}
